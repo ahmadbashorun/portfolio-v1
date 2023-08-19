151605ee447e6aba5b8133b450dbbcb527a09736
Ahmad-Tijani Bashorun <ahmadbashorun@gmail.com>
2023-08-19 23:58:00 +0100

    fixed responsive nav menu bug

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ let navMenu = document.getElementById("nav-menu")
 let closeNavIcon = document.getElementById("close-nav-icon")
 
 const openNavMenu = ()=>{
-    navMenu.classList.remove("nav-menu-dropdown")
     navMenu.classList.add("nav-menu-shown" )
 }
 
 openNavIcon.addEventListener('click', openNavMenu)
 
 const closeNavMenu = () =>{
-    if(!navMenu.classList.contains("nav-menu-dropdown")){
-        navMenu.classList.remove("nav-menu-shown")
-        navMenu.classList.add("nav-menu-dropdown")
-    }
+    navMenu.classList.remove("nav-menu-shown")
+
 }
 
 closeNavIcon.addEventListener('click', closeNavMenu)
+
